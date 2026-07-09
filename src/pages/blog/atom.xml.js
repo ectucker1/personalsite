12932e537aaf36f777a2c37e19a61a89cbc13a59
Ella Tucker <ectucker1@gmail.com>
2026-07-08 20:49:38 -0500

    Make the Atom feed actually an Atom feed

--- src/pages/blog/atom.xml.js
+++ src/pages/blog/atom.xml.js
@@ -1,19 +1,43 @@
-import rss from "@astrojs/rss";
+import { Feed } from "feed";
 import { getCollection } from "astro:content";
 import { postUrl } from "@data/post";
 
 export async function GET(context) {
-  const blog = await getCollection("blog");
-  return rss({
+  const site = context.site;
+  const posts = (await getCollection("blog")).sort(
+    (a, b) => b.data.date.valueOf() - a.data.date.valueOf(),
+  );
+
+  const feedUrl = new URL("/blog/atom.xml", site).toString();
+
+  const feed = new Feed({
     title: "ectcetera",
+    author: {
+      name: "Ella Tucker",
+      link: "https://ectcetera.net",
+    },
     description: "Experiments in gamedev and more.",
-    site: context.site,
-    items: blog.map((post) => ({
+    id: feedUrl,
+    link: site.toString(),
+    language: "en-us",
+    feedLinks: {
+      atom: feedUrl,
+    },
+    updated: posts.length ? posts[0].data.date : undefined,
+  });
+
+  for (const post of posts) {
+    const link = new URL(postUrl(post), site).toString();
+    feed.addItem({
       title: post.data.title,
-      pubDate: post.data.pubDate,
+      id: link,
+      link,
       description: post.data.tagline,
-      link: postUrl(post),
-    })),
-    customData: `<language>en-us</language>`,
+      date: post.data.date,
+    });
+  }
+
+  return new Response(feed.atom1(), {
+    headers: { "Content-Type": "application/xml; charset=utf-8" },
   });
 }
